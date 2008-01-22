e5182bc9b13676e353fa25f7a962472a353ff51f
kasatani <kasatani@8bd1c5d2-8343-0410-a987-db9d25464fa8>
2008-01-22 05:46:22 +0000

    rewritten extract_samples script in C

---
+++ PocketGuitar/extract_samples.m
@@ -0,0 +1,85 @@
+#import <Foundation/Foundation.h>
+#include <mpg123.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#define BUFSIZE 8192
+
+NSString *baseDir = @"/var/root/Media/PocketGuitar";
+int extractSamples();
+int convertMp3ToRaw(NSString *from, NSString *to);
+
+int main(int argc, char *argv[]) {
+	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+	int ret = extractSamples();
+	[pool release];
+	return ret;
+}
+
+int extractSamples() {
+	int ret;
+	mpg123_init();
+	
+	NSFileManager *manager = [NSFileManager defaultManager];
+	NSDirectoryEnumerator *enm = [manager enumeratorAtPath:baseDir];
+	NSString *file, *path;
+	while ((file = [enm nextObject])) {
+		if ([[file pathExtension] isEqualToString:@"mp3"]) {
+			path = [baseDir stringByAppendingPathComponent:file];
+			if ((ret = convertMp3ToRaw(path, [[path stringByDeletingPathExtension] stringByAppendingPathExtension:@"raw"]))) {
+				return ret;
+			}
+		}
+	}
+	return 0;
+}
+
+int convertMp3ToRaw(NSString *from, NSString *to) {
+	mpg123_handle *mh;
+	int error;
+	int outfd;
+	int ret;
+	int result = 0;
+	size_t done;
+	unsigned char out[BUFSIZE];
+
+	NSLog(@"Converting %@ to %@", from, to);
+	
+	mh = mpg123_new(NULL, &error);
+	if (MPG123_OK != mpg123_open(mh, (char *)[from UTF8String])) {
+		NSLog(@"failed to open file: %@", from);
+		result = 1;
+		goto error;
+	}
+	
+	outfd = open([to UTF8String], O_WRONLY|O_CREAT, 0644);
+	if (outfd < 0) {
+		NSLog(@"failed to open file: %@", to);
+		result = 1;
+		goto close_in;
+	}
+	while (1) {
+		ret = mpg123_read(mh, out, BUFSIZE, &done);
+		if (MPG123_DONE == ret) {
+			break;
+		} else if (ret <= 0) {
+			ret = write(outfd, out, done);
+			if (ret < 0) {
+				NSLog(@"failed to write to file: %@", to);
+				result = 1;
+				goto close_all;
+			}
+		} else {
+			NSLog(@"failed to read file: %@", from);
+			result = ret;
+			goto close_all;
+		}
+	}
+	
+close_all:
+	close(outfd);
+close_in:
+	mpg123_close(mh);
+error:
+	return result;
+}
